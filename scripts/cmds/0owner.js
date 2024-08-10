0bb29f714e20f9049e6570ad00a1e38c4732ba99
GodAlwaysone <157789425+GodAlwaysone@users.noreply.github.com>
2024-08-10 17:14:55 +0545

    Update 0owner.js

--- scripts/cmds/0owner.js
+++ scripts/cmds/0owner.js
@@ -16,12 +16,12 @@ config: {
   onStart: async function ({ api, event }) {
   try {
     const ownerInfo = {
-      name: 'Yazky Ai',
+      name: 'Alawaysone',
       gender: 'Male',
       age: 'Unknown',
       height: 'Unknown',
-      facebookLink: 'stalk moko mwa',
-      nick: 'Yazky'
+      facebookLink: 'not available',
+      nick: 'Kiyo chan'
     };
 
     const bold = 'https://i.imgur.com/DDO686J.mp4'; // Replace with your Google Drive videoid link https://drive.google.com/uc?export=download&id=here put your video id
